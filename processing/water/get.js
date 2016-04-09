2f3900014a01cd32cbb637d476d305818ddebe27
Bill Morris <wboykinm@geosprocket.com>
2016-04-08 20:27:09 -0400

    pull mapzen tiles

--- processing/water/get.js
+++ processing/water/get.js
@@ -1,34 +1,43 @@
-// SPIT OUT A NAMED GEOJSON FILE FOR EACH INPUT TILE ADDRESS
+/ SPIT OUT A NAMED GEOJSON FILE FOR EACH INPUT TILE ADDRESS
 
 var fs = require('fs');
 var request = require('request');
 var turf = require('turf');
-var Pbf = require('pbf');
-var geobuf = require('geobuf');
-
 
 var bigTile = process.argv[2];
 var outDir = process.argv[3];
-var mbToken = process.argv[4];
-var mbUrlBase = 'http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/';
-var mbUrlSuffix = '.vector.pbf?access_token=' + mbToken;
+var mzUrlBase = 'http://vector.mapzen.com/osm/water/';
+var mzUrlSuffix = '.json?api_key=vector-tiles-TE70otI';
 
 var x = JSON.parse(bigTile)[0];
 var y = JSON.parse(bigTile)[1];
 var z = JSON.parse(bigTile)[2];
 
-var getTile = request(mbUrlBase + z + '/' + x + '/' + y + mbUrlSuffix, function (error, response, body) {
+var getTile = request(mzUrlBase + z + '/' + x + '/' + y + mzUrlSuffix, function (error, response, body) {
   if (error) {
     console.error('encountered error', error instanceof Error ? error.stack : error);
   } else if (response.statusCode === 429) {
     console.log('error 429');
   } else if (response.statusCode !== 200) {
     console.error('non-200 status code: ' + response.statusCode);
+  } else if (JSON.parse(body).features.length > 0 ) {
+    body = JSON.parse(body);
+    var outWater = {"type":"FeatureCollection","features":[]};
+    for (var p = 0; p < body.features.length; p++) {
+      if (body.features[p].geometry.type == 'Polygon' || body.features[p].geometry.type == 'MultiPolygon') {
+        if (turf.kinks(body.features[p]).intersections.features.length > 0) {
+          console.log('Self-intersecting polygon detected and removed. Intersection(s) at: ');
+          console.log(JSON.stringify(turf.kinks(body.features[p]).intersections));
+        }
+        else {
+          outWater.features.push(body.features[p]);
+        }
+      }
+    }
+    fs.writeFile(outDir + 'osm_water_' + x + '_' + y + '_' + z + '.geojson', JSON.stringify(outWater), 'utf-8');
+    console.log('got tile ' + bigTile);
   } else {
-    //console.log(body);
-    var geojson = geobuf.decode(new Pbf(body));
-    console.log(geojson);
-    //geojson = JSON.parse(geojson);
+    console.log('no water features');
   }
 });
 
