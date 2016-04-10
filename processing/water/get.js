3c22f6b26b84e4bb9ecb5c1d0a2c69b491bf7bbe
Bill Morris <wboykinm@geosprocket.com>
2016-04-09 22:08:56 -0400

    use vector-tile-js

--- processing/water/get.js
+++ processing/water/get.js
@@ -4,19 +4,19 @@ var fs = require('fs');
 var request = require('request');
 var turf = require('turf');
 var Protobuf = require('pbf');
-var VectorTile = require('vector-tile').VectorTile;;
+var VectorTile = require('vector-tile').VectorTile;
 
 var bigTile = process.argv[2];
 var outDir = process.argv[3];
 var mbToken = process.argv[4];
-var mbUrlBase = 'http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/';
+var mbUrlBase = 'http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v5/';
 var mbUrlSuffix = '.vector.pbf?access_token=' + mbToken;
 
 var x = JSON.parse(bigTile)[0];
 var y = JSON.parse(bigTile)[1];
 var z = JSON.parse(bigTile)[2];
 
-var getTile = request(mbUrlBase + z + '/' + x + '/' + y + mbUrlSuffix, function (error, response, body) {
+var getTile = request({url: mbUrlBase + z + '/' + x + '/' + y + mbUrlSuffix, gzip: true, encoding: null}, function (error, response, body) {
   if (error) {
     console.error('encountered error', error instanceof Error ? error.stack : error);
   } else if (response.statusCode === 429) {
@@ -24,10 +24,21 @@ var getTile = request(mbUrlBase + z + '/' + x + '/' + y + mbUrlSuffix, function 
   } else if (response.statusCode !== 200) {
     console.error('non-200 status code: ' + response.statusCode);
   } else {
-    //console.log(body);
-    var geojson = geobuf.decode(new Pbf(body));
-    console.log(geojson);
-    //geojson = JSON.parse(geojson);
+    var buf = new Protobuf(body);
+    var vt = new VectorTile(buf);
+    var layer = vt.layers['water'];
+    
+    if (layer.length > 0) {
+      var waterSet = {"type":"FeatureCollection","features":[]};
+      for (var i = 0; i < layer.length; i++) {
+        var feature = layer.feature(i).toGeoJSON();      
+        waterSet.features.push(feature);
+      }
+      console.log(JSON.stringify(waterSet));
+    }
+    else {
+      console.log('no water features at tile {' + z + '}/{' + x + '}/{' + y + '}')
+    }
   }
 });
 
