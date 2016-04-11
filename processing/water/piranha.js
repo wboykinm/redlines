b2d6398afc50d01b713b956adfc84de15c1290ed
Bill Morris <wboykinm@geosprocket.com>
2016-04-11 15:35:52 -0400

    buffer strategies

--- processing/water/piranha.js
+++ processing/water/piranha.js
@@ -9,7 +9,7 @@ var tractsEaten = {"type":"FeatureCollection","features":[]};
 
 console.log('Processing ' + process.argv[3] + ' with ' + piranhas.features.length + ' water features');
 
-if (piranhas.features) {
+if (piranhas.features.length > 0) {
   // Isolate the water polygons from all that other osm noise
   for (var p = 0; p < piranhas.features.length; p++) {
     if (piranhas.features[p].geometry.type == 'Polygon' || piranhas.features[p].geometry.type == 'MultiPolygon') {
@@ -17,13 +17,22 @@ if (piranhas.features) {
     } 
   }
 
+  try {
+    // expand everything by 50m to catch intersections
+    // piranhaPolys = turf.buffer(piranhaPolys,0.05,'kilometers');
+    // dissolve the water features
+    piranhaPolys = turf.merge(piranhaPolys);
+  }
+  catch (e) {
+    console.log(e);
+  }
+
   // loop through all tracts looking for intersection
   for (var i = 0; i < tractPolys.features.length; i++) {
     // chomp water from tract polygon if intersecting
-    //console.log('Running '+ tractPolys.features[i].properties.TRACTCE)
-    if (turf.intersect(piranhaPolys.features[0],tractPolys.features[i])) {
+    if (turf.intersect(piranhaPolys,tractPolys.features[i])) {
       console.log('Tract ' + tractPolys.features[i].properties.TRACTCE + ' is in the water. Chomping . . .')
-      tractsEaten.features.push(turf.erase(tractPolys.features[i],piranhaPolys.features[0]));
+      tractsEaten.features.push(turf.erase(tractPolys.features[i],piranhaPolys));
     } 
     // otherwise just write the tract polygon as is
     else {
